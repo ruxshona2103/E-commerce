96c724df95bbcdd3e8390d2e0974b4d8b1cc1d6a
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-02-17 22:55:26 +0500

    urls added

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ProductViewSet, ReviewViewSet, CategoryViewSet
 from services.flash_sale import check_flash_sale, FlashSaleListCreateView
+from  services.product_view_history import ProductViewHistoryCreate
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -15,5 +16,6 @@ urlpatterns = [
 
     path('sale/', FlashSaleListCreateView.as_view(), name='sale'),
     path('check-sale/<int:product_id>/', check_flash_sale, name='product-view-history-create'),
+    path('product-view/', ProductViewHistoryCreate.as_view(), name='product-view-history-create'),
 
 ]
